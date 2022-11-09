711aaddabeba1cc1d3be1452d7f67d808f2f431b
Timmo <timmo.n@itcgroup.io>
2022-11-09 10:58:19 +0700

    Thinh ngu 2

--- cat.php
+++ cat.php
@@ -14,7 +14,38 @@ spl_autoload_register('loadClass');
         <?php
             $obj = new Category();
             $data = $obj->all();
-            print_r($data);
         ?>
+        <table>
+            <tr>
+                <td>Id</td>
+                <td>Name</td>
+                <td>#</td>
+            </tr>
+            <?php
+                foreach($data as $item) {
+                    ?>
+                        <tr>
+                            <td>
+                                <?php
+                                    echo $item['cat_id']
+                                ?>
+                            </td>
+
+                            <td>
+                                <?php
+                                    echo $item['cat_name']
+                                ?>
+                            </td>
+
+                            <td>
+                                <a href="cat_delete.php?id=<?php echo $item['cat_id'] ?>">
+                                    Xóa
+                                </a>
+                            </td>
+                        </tr>
+                    <?php
+                }
+            ?>
+        </table>
     </body>
 </html>
